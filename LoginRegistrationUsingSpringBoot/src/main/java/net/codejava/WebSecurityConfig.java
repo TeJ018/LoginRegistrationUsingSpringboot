5dcb04e6d0ce43f12e0ff0d12dc81ad31b10e57b
TeJ018 <129145415+TeJ018@users.noreply.github.com>
2023-03-28 19:57:44 -0400

    Delete WebSecurityConfig.java

--- LoginRegistrationUsingSpringBoot/src/main/java/net/codejava/WebSecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package net.codejava;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-}
